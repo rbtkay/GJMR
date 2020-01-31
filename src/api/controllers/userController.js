5b099ce76f77072dc423c9136fce10f22613eb0f
rbtkay <robert.g.khayat@gmail.com>
2020-01-31 12:48:07 +0100

    les mots de passe sont encryptes dans la base de donnees

--- src/api/controllers/userController.js
+++ src/api/controllers/userController.js
@@ -1,25 +1,35 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 exports.user_login = (request, response) => {
-    let { body } = request;
-    
-    console.log('body');
-    console.log(body);
+    let { email, password } = request.body;
 
-    User.findOne(body, (error, user) => {
-        jwt.sign({ email: user.email, role: user.role }, process.env.JWT_KEY, { expiresIn: "10m" }, (jwtError, token) => {
-            if (jwtError) {
-                console.log(jwtError);
-                response.status(500);
-                response.json({ message: "Erreur serveur" });
-            }
-            else {
-                response.status(200);
-                response.json({ token });
-            }
-        })
+    console.log('password')
+    console.log(password)
+
+
+    User.findOne({ email }, (error, user) => {
+        console.log("userPassword");
+        console.log(user.password);
+
+        const isPasswordCorrect = bcrypt.compareSync(password, user.password)
+        if (!isPasswordCorrect) {
+            response.status(401);
+            response.json({ message: 'Auth Failed' });
+        } else {
+            jwt.sign({ email: user.email, role: user.role }, process.env.JWT_KEY, { expiresIn: "10m" }, (jwtError, token) => {
+                if (error) {
+                    response.status(500);
+                    response.json({ message: "Erreur serveur" });
+                }
+                else {
+                    response.status(200);
+                    response.json({ token });
+                }
+            })
+        }
     })
 }
 
--- src/api/middlewares/jwtMiddleware.js
+++ src/api/middlewares/jwtMiddleware.js
@@ -1,13 +1,11 @@
 const jwt = require('jsonwebtoken');
 
 exports.verify_token = (req, res, next) => {
-  // let token = req.headers['authorization'];
-  let token = req.headers.authorization;
+  let token = req.headers['authorization'];
 
   if(token){
     jwt.verify(token, process.env.JWT_KEY, (error, result) => {
       if(error){
-        // res.sendStatus(403);
         res.status(403);
         res.json({message: "Accès refusé"})
       }
