84f033785502c182c73f00d9eb052c49d6df98da
GeoDaz <gfrydz2@gmail.com>
2020-02-08 13:15:51 +0100

    Fix getModulesByTeacher params Set user_id in login return

--- src/api/routes/modulesInSchoolYearRoutes.js
+++ src/api/routes/modulesInSchoolYearRoutes.js
@@ -3,8 +3,9 @@ const module_in_schoolyear_controller = require("../controllers/moduleOfSchoolYe
 module.exports = app => {
     app.route("/module_in_schoolyear/")
         .post(module_in_schoolyear_controller.addModuleToSchoolYear)
-        .delete(module_in_schoolyear_controller.removeModuleFromSchoolYear)
-        .get(/* jwtMiddleware.verify_token, */ module_in_schoolyear_controller.getAllModulesInYear);
+        .delete(module_in_schoolyear_controller.removeModuleFromSchoolYear);
+    app.route("/module_in_schoolyear/:school_year_id")
+        .get(/* jwtMiddleware.verify_token, */ module_in_schoolyear_controller.getModulesInYear);
     // .patch(/* jwtMiddleware.verify_token, */ note_controllers.updateNote)
     // .delete(/* jwtMiddleware.verify_token, */ note_controllers.deleteNote);
 };
